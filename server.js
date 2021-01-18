15d31b79d4258d14bff1884e4badb344cae6847d
Pdnarmi <pdnarmi@ncsu.edu>
2021-01-18 16:56:12 -0500

    merged html routes into server.js, moved seed file into seeders

--- server.js
+++ server.js
@@ -2,15 +2,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-const apiRoutes = ;
-const htmlRoutes= ;
+const apiRoutes = require('./routes/apiRoutes');
+
 
 app.use(apiRoutes);
-app.use(htmlRoutes);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
@@ -18,3 +17,19 @@ app.use(express.static("public"));
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
 
 mongoose.connect(MONGODB_URI);
+
+app.get("/exercise", function (req, res) {
+    res.sendFile(path.join(__dirname, "./public/exercise.html"));
+  });
+  
+  app.get("/stats", function (req, res) {
+    res.sendFile(path.join(__dirname, "./public/stats.html"));
+  });
+  
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+  });
+  
+  app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+  });
